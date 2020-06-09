bb1012d1376ce4453a38c14d7b2e32b7306ca2fc
Jeffery Shivers <jefferyshivers@gmail.com>
2020-06-09 13:51:13 -0400

    init

---
+++ src/main/java/protop/gradle/plugin/ProtopExtension.java
@@ -0,0 +1,9 @@
+package protop.gradle.plugin;
+
+import org.gradle.api.plugins.ExtensionContainer;
+
+public class ProtopExtension {
+
+    public boolean refreshGitSources = false;
+    public boolean useLinks = false;
+}
